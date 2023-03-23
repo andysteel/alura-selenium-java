2644c870fa37ee11ab62d7bfea171a16cd0ff9e7
Anderson Dias <andersoninfonet@gmial.com>
2023-03-23 13:24:16 -0300

    transform LoginTest to use Page model

---
+++ src/test/java/br/com/alura/leilao/login/LoginPage.java
@@ -0,0 +1,66 @@
+package br.com.alura.leilao.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LoginPage {
+
+    private static final String HTTP_LOGIN = "http://localhost:8080/login";
+    private static final String HTTP_LOGIN_ERROR = "http://localhost:8080/login?error";
+    private WebDriver browser;
+
+    public LoginPage() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-debugging-port=9222");
+
+        this.browser = new ChromeDriver(options);
+        this.browser.navigate().to(HTTP_LOGIN);
+    }
+
+    public void fechar() {
+        this.browser.quit();
+    }
+
+    public void preencherFormularioDeLogin(String username, String password) {
+        this.browser.findElement(By.id("username")).sendKeys(username);
+        this.browser.findElement(By.id("password")).sendKeys(password);
+    }
+
+    public void efetuaLogin() {
+        this.browser.findElement(By.id("login-form")).submit();
+    }
+
+    public boolean isPaginaDeLogin() {
+        return this.browser.getCurrentUrl().equals(HTTP_LOGIN);
+    }
+
+    public boolean isPaginaDeLoginComError() {
+        return this.browser.getCurrentUrl().equals(HTTP_LOGIN_ERROR);
+    }
+
+    public boolean paginaContemMensagemDeErroDeLogin() {
+        return browser.getPageSource().contains("Usuário e senha inválidos.");
+    }
+
+    public boolean paginaContemTextoDePaginaDeLeiloes() {
+        return browser.getPageSource().contains("Dados do Leilão");
+    }
+
+    public String getNomeUsuarioLogado() {
+        try {
+            return this.browser.findElement(By.id("usuario-logado")).getText();
+        } catch (NoSuchElementException ex) {
+            return null;
+        }
+    }
+
+    public void navegaParaPaginaDeLances() {
+        this.browser.navigate().to("http://localhost:8080/leiloes/2");
+    }
+}
--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -1,73 +1,52 @@
 package br.com.alura.leilao.login;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginTest {
 
-    public static final String HTTP_LOCALHOST_8080_LOGIN = "http://localhost:8080/login";
-    private WebDriver browser;
-
-    @BeforeAll
-    public static void beforeAll() {
-        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver");
-    }
+    private LoginPage paginaDeLogin;
 
     @BeforeEach
     void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-debugging-port=9222");
-
-        this.browser = new ChromeDriver(options);
-        this.browser.navigate().to(HTTP_LOCALHOST_8080_LOGIN);
+        this.paginaDeLogin = new LoginPage();
     }
 
     @AfterEach
     void afterEach() {
-        this.browser.quit();
+        this.paginaDeLogin.fechar();
     }
 
     @Test
     void deveEfetuarLoginComDadosValidos() {
 
-        this.browser.findElement(By.id("username")).sendKeys("fulano");
-        this.browser.findElement(By.id("password")).sendKeys("pass");
-        this.browser.findElement(By.id("login-form")).submit();
+        this.paginaDeLogin.preencherFormularioDeLogin("fulano", "pass");
+        this.paginaDeLogin.efetuaLogin();
 
-        assertNotEquals(HTTP_LOCALHOST_8080_LOGIN, this.browser.getCurrentUrl());
-        assertEquals("fulano", this.browser.findElement(By.id("usuario-logado")).getText());
+        assertFalse(this.paginaDeLogin.isPaginaDeLogin());
+        assertEquals("fulano", this.paginaDeLogin.getNomeUsuarioLogado());
     }
 
     @Test
     void naoDeveEfetuarLoginComDadosInvalidos() {
 
-        this.browser.findElement(By.id("username")).sendKeys("invalido");
-        this.browser.findElement(By.id("password")).sendKeys("pass");
-        this.browser.findElement(By.id("login-form")).submit();
+        this.paginaDeLogin.preencherFormularioDeLogin("invalido", "pass");
+        this.paginaDeLogin.efetuaLogin();
 
-        By by = By.id("usuario-logado");
 
-        assertEquals("http://localhost:8080/login?error", browser.getCurrentUrl());
-        assertTrue(browser.getPageSource().contains("Usuário e senha inválidos."));
-        assertThrows(NoSuchElementException.class, () -> this.browser.findElement(by));
+        assertTrue(this.paginaDeLogin.isPaginaDeLoginComError());
+        assertTrue(this.paginaDeLogin.paginaContemMensagemDeErroDeLogin());
+        assertNull(this.paginaDeLogin.getNomeUsuarioLogado());
     }
 
     @Test
     void naoDeveriaAcessarPaginaRestritaSemEstarLogado() {
-        this.browser.navigate().to("http://localhost:8080/leiloes/2");
+        this.paginaDeLogin.navegaParaPaginaDeLances();
 
-        assertEquals(HTTP_LOCALHOST_8080_LOGIN, this.browser.getCurrentUrl());
-        assertFalse(this.browser.getPageSource().contains("Dados do Leilão"));
+        assertTrue(this.paginaDeLogin.isPaginaDeLogin());
+        assertFalse(this.paginaDeLogin.paginaContemTextoDePaginaDeLeiloes());
     }
 }
